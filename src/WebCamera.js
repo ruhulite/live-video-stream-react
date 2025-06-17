83fa96432ed0ad2e7888681eb0482367f404cff1
ruhulite <corporate.ruhul@gmail.com>
2025-06-18 02:13:44 +0600

    take selfie and validate with captcha

--- src/WebCamera.js
+++ src/WebCamera.js
@@ -12,6 +12,37 @@ const WebCamera = () => {
     const shape = useRef(null);
     const captchaRef = useRef(null);
     const [imgUrl, setImgUrl] = useState(null);
+    const [validationTitleIndex, setValidationTitleIndex] = useState(0);
+    const [validationMessage, setValidationMessage] = useState(null);
+
+    const validationDataSet = ['triangle', 'circle', 'square'];
+    const captchaData = [
+        '',
+        'triangle',
+        'circle',
+        'square',
+        '',
+        'triangle',
+        'circle',
+        'square',
+        '',
+        'triangle',
+        'circle',
+        'square',
+        '',
+        'triangle',
+        'circle',
+        'square',
+        '',
+        'triangle',
+        'circle',
+        'square',
+        '',
+        'triangle',
+        'circle',
+        'square',
+        ''
+    ];
 
     // rectangle box animation
     let x = 0;
@@ -20,7 +51,7 @@ const WebCamera = () => {
     let ySpeed = 1;
 
     function animate() {
-        // Boundary check
+        // check boundary area
         if (shape.current) {
             if (x + shape.current.offsetWidth > 400 || x < 0) {
                 xSpeed = -xSpeed;
@@ -29,7 +60,7 @@ const WebCamera = () => {
                 ySpeed = -ySpeed;
             }
 
-            // Update position
+            // update position
             x += xSpeed;
             y += ySpeed;
 
@@ -45,19 +76,73 @@ const WebCamera = () => {
         animate();
     }, [shape]);
 
+    // capture selfie image
     const captureImage = useCallback(async () => {
+        setImgUrl(null);
+        setValidationMessage(null);
         const imgSrc = camRef.current.getScreenshot();
         setImgUrl(imgSrc);
 
         setTimeout(() => {
             captchaRef.current.style.top = shape.current.style.top;
             captchaRef.current.style.left = shape.current.style.left;
-        }, 1)
+        }, 1);
+
+        setValidationTitleIndex(Math.floor(Math.random() * validationDataSet.length));
     }, [camRef]);
+
+    // making random captcha data
+    function makeArrayShuffle(arr) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]]; // swap array data
+        }
+        return arr;
+    }
+
+    const randomDataObject = makeArrayShuffle([...captchaData]);
+
+    let selectedCaptchaData = [];
+
+    // click captcha box
+    const handleClick = (val) => {
+        val.e.target.parentNode.classList.add('active');
+        selectedCaptchaData.push(val.item);
+    }
+
+    // handle validation & set validation message
+    const handleValidation = () => {
+        if (selectedCaptchaData.length < 1) {
+            setValidationMessage(`Please select ${validationDataSet[validationTitleIndex]}`);
+        } else {
+            let wrongData = undefined;
+            for (let i = 0; i < selectedCaptchaData.length; i++) {
+                if (selectedCaptchaData[i] !== validationDataSet[validationTitleIndex]) {
+                    wrongData = true;
+                }
+            }
+            if (wrongData) {
+                setValidationMessage('Not match! Please try again.')
+                selectedCaptchaData = [];
+                captchaRef.current.querySelectorAll('li').forEach((element) => element.classList.remove('active'));
+            } else {
+                if (selectedCaptchaData.length >= 3) {
+                    setValidationMessage('Validation success.')
+                    selectedCaptchaData = [];
+                    captchaRef.current.querySelectorAll('li').forEach((element) => element.classList.remove('active'));
+                } else {
+                    setValidationMessage(`Please select at least 3 ${validationDataSet[validationTitleIndex]}`);
+                    selectedCaptchaData = [];
+                    captchaRef.current.querySelectorAll('li').forEach((element) => element.classList.remove('active'));
+                }
+            }
+        }
+    }
 
     return (
         <>
             <div className="web-camera-video">
+                <h3>Take Selfie</h3>
                 <div className="camera-wrapper">
                     <div ref={shape} className="rectangle-box" />
                     <Webcam
@@ -67,15 +152,42 @@ const WebCamera = () => {
                         audio={false}
                     />
                     <div className="action-button">
-                        <button onClick={captureImage}>Capture Image</button>
-                        <button className="refresh" onClick={() => setImgUrl(null)}>Refresh</button>
+                        <button onClick={captureImage}>Continue</button>
+                        <button className="refresh" onClick={() => {
+                            setImgUrl(null);
+                            setValidationMessage(null);
+                        }}>Refresh</button>
                     </div>
                 </div>
                 <div className="preview-image">
                     {imgUrl && (
                         <>
-                            <img src={imgUrl} alt="Captured Screnshot" />
-                            <div ref={captchaRef} className="captcha-rectangle">Captcha</div>
+                            <h3>Select {validationDataSet[validationTitleIndex]}</h3>
+                            <p className={validationMessage === 'Validation success.' ? 'success' : ''}>{validationMessage}</p>
+                            <div className="preview-image-wrap">
+                                <img src={imgUrl} alt="Captured Screnshot" />
+                                <ul ref={captchaRef} className="captcha-rectangle">
+                                    { randomDataObject.map((item, index) =>
+                                        (<li
+                                            key={index}
+                                            onClick={(e) => handleClick({item, e})}
+                                        >
+                                            <span className="checked">&#10004;</span>
+                                            { item === 'circle' ? (
+                                                <span className="circle" />
+                                            ) : item === 'square' ? (
+                                                <span className="square" />
+                                            ) : item === 'triangle' ? (
+                                                <span className="triangle" />
+                                            ) : (
+                                                <span className="blank" />
+                                            ) }
+                                        </li>)) }
+                                </ul>
+                            </div>
+                            <div className='action-button'>
+                                <button onClick={() => handleValidation() }>Validate</button>
+                            </div>
                         </>
                     )}
                 </div>
